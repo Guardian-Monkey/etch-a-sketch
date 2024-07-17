7d9610d3dfc4441ec00bb88d463ce17dfbb7c123
Paul Gutierrez <paulitoguti747@gmail.com>
2024-07-17 16:24:23 -0400

    Remove comments at the top of js file

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Create a 16 X 16 grid of squares using js
-// It's best to put these squares within a container div in the html
-// Use Flexbox to make the divs appear as a grid
-// Be careful when using borders and margins, as they can adjust the size of the squares
-
 const container = document.querySelector("#container");
 function createBoard(dimensions) {
     for (let i = 0; i < dimensions; i++) {
